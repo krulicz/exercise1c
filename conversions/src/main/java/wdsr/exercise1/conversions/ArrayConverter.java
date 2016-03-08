c31f6bdf2010fdf2156f24d1f7798fdacac98e82
Jakub Kulik <krulicz@interia.pl>
2016-03-08 11:49:44 +0100

    Added exericse1c

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,19 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	private static final Logger log = LogManager.getLogger();
+	int [] wynik[];
     public int[] convertToInts(String[] strings) {
-        return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+    	log.debug("convertToInts starting with parameters: ", Arrays.toString(strings));
+    	int wynik[] = Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+    	log.debug("convertToInts exiting with : ", Arrays.toString(wynik));
+        return wynik;
     }
 }
